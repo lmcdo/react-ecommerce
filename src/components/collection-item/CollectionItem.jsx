ab9a853de3385e93348978f597b3b475e2b4883f
Italo Felipe <felipe.italo@hotmail.com>
2019-07-26 16:26:09 -0300

    Fim da aula 107 - Reducer de adicionar itens ao carrinho implementado. Ainda existem melhorias para se fazer nele.

--- src/components/collection-item/CollectionItem.jsx
+++ src/components/collection-item/CollectionItem.jsx
@@ -1,8 +1,16 @@
+// Dependencias
 import React from "react";
+import { connect } from "react-redux";
+// Componentes
 import CustomButton from "../custom-buttom/CustomButton";
+
+//Redux
+import { addItem } from "../../redux/cart/cartActions";
+// CSS
 import "./collection-item.scss";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => {
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
   return (
     <div className="collection-item">
       <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
@@ -10,9 +18,17 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <CustomButton inverted>Add to cart</CustomButton>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        Add to cart
+      </CustomButton>
     </div>
   );
 };
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+});
+export default connect(
+  null,
+  mapDispatchToProps
+)(CollectionItem);
--- src/redux/cart/cartReducer.js
+++ src/redux/cart/cartReducer.js
@@ -1,7 +1,8 @@
 import CartActionTypes from "./CartTypes";
 
 const INITIAL_STATE = {
-  hidden: true
+  hidden: true,
+  cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -10,6 +11,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...SVGMetadataElement,
         hidden: !state.hidden
+      };
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: [...state.cartItems, action.payload]
       };
     default:
       return state;
