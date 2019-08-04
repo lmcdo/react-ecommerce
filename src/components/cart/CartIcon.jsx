3074dccf0959b83353fdae2505ad7d5e5193ed6b
Italo Felipe <felipe.italo@hotmail.com>
2019-08-04 14:52:33 -0300

    Fim da aula 112: Contador de itens no carrinho de compras adicionado.

--- src/components/cart/CartIcon.jsx
+++ src/components/cart/CartIcon.jsx
@@ -6,11 +6,11 @@ import { connect } from "react-redux";
 import { toggleCartHidden } from "../../redux/cart/cartActions";
 // CSS
 import "./cart-icon.scss";
-const CartIcon = ({ toggleCartHidden }) => {
+const CartIcon = ({ toggleCartHidden, itemCount }) => {
   return (
     <div className="cart-icon" onClick={toggleCartHidden}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{itemCount}</span>
     </div>
   );
 };
@@ -18,8 +18,14 @@ const CartIcon = ({ toggleCartHidden }) => {
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce(
+    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
+    0
+  )
+});
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(CartIcon);
